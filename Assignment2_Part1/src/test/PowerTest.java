ce2efb8b958bbf5cde77a592f5ceb877f8453a88
Stephane_KI <ki.stephane.dambi@gmail.com>
2014-11-14 17:46:33 +0200

    Test added to kill mutants and reach a mutant coverage of 90

--- Assignment2_Part1/src/test/PowerTest.java
+++ Assignment2_Part1/src/test/PowerTest.java
@@ -1,15 +1,29 @@
 package test;
 
+import src.Power;
 import junit.framework.TestCase;
 
 public class PowerTest extends TestCase {
 
-	protected void setUp() throws Exception {
-		super.setUp();
+	public void testIfRightIsZeroResultIsOne() {
+		assertEquals(1, Power.power(-5, 0));
 	}
-
-	public void testPower() {
-		fail("Not yet implemented");
+	
+	public void testIfLeftIsZeroResultIsZero() {
+		assertEquals(0, Power.power(0, 12));
+	}
+	
+	
+	public void testIfRightEqualsToOneThenResultEqualLeft() {
+		assertEquals(3, Power.power(3, 1));
+	}
+	
+	public void testPostConditionIsVerified() {
+		assertEquals(27, Power.power(3, 3));
+	}
+	
+	public void testPostConditionIsVerified2() {
+		assertEquals(9, Power.power(3, 2));
 	}
 
 }
